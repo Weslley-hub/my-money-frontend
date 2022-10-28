83ec464ad2b115885d1185db5edb3ba0f5714b3f
JorgeNobre20 <jorgenobre1234@gmail.com>
2022-10-28 20:39:04 -0300

    troca importação direta de opções de avatar na constantes de avatares pela importação a partir da exportação do index da pasta de avatares

--- src/components/AvatarSelector/utils/defaultAvatarOptions.ts
+++ src/components/AvatarSelector/utils/defaultAvatarOptions.ts
@@ -1,8 +1,10 @@
-import manAvatarImg1 from "../../../assets/images/icons/avatars/man_avatar_1.svg";
-import manAvatarImg2 from "../../../assets/images/icons/avatars/man_avatar_2.svg";
-import manAvatarImg3 from "../../../assets/images/icons/avatars/man_avatar_3.svg";
-import womanAvatarImg1 from "../../../assets/images/icons/avatars/woman_avatar_1.svg";
-import womanAvatarImg2 from "../../../assets/images/icons/avatars/woman_avatar_2.svg";
+import {
+  manAvatar1,
+  manAvatar2,
+  manAvatar3,
+  womanAvatar1,
+  womanAvatar2,
+} from "../../../assets/images/avatars";
 
 export type Avatar = {
   id: string;
@@ -13,27 +15,27 @@ export type Avatar = {
 export const defaultAvatarOptions: Avatar[] = [
   {
     alternativeDescription: "Avatar 1 feminino",
-    imgSource: womanAvatarImg1,
+    imgSource: womanAvatar1,
     id: "WOMAN_AVATAR_1",
   },
   {
     alternativeDescription: "Avatar 2 feminino",
-    imgSource: womanAvatarImg2,
+    imgSource: womanAvatar2,
     id: "WOMAN_AVATAR_2",
   },
   {
     alternativeDescription: "Avatar 1 masculino",
-    imgSource: manAvatarImg1,
+    imgSource: manAvatar1,
     id: "MAN_AVATAR_1",
   },
   {
     alternativeDescription: "Avatar 2 masculino",
-    imgSource: manAvatarImg2,
+    imgSource: manAvatar2,
     id: "MAN_AVATAR_2",
   },
   {
     alternativeDescription: "Avatar 3 masculino",
-    imgSource: manAvatarImg3,
+    imgSource: manAvatar3,
     id: "MAN_AVATAR_3",
   },
 ];
