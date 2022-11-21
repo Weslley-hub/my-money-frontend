23ae24f6f5d7605259017397ef3dbe95415ec644
JorgeNobre20 <jorgenobre1234@gmail.com>
2022-11-21 20:50:22 -0300

    tira tela de home e categorias das rotas de autenticaçao

--- src/routes/Auth.routes.tsx
+++ src/routes/Auth.routes.tsx
@@ -4,18 +4,14 @@ import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { ForgetPassword } from "../pages/ForgetPassword";
 import { ForgetPasswordEmail } from "../pages/ForgetPasswordEmail";
-import { Home } from "../pages/Home";
-import { Categories } from "../pages/Categories";
 
 export function AuthRoutes() {
   return (
     <Routes>
       <Route path="/auth/login" element={<Login />} />
-      <Route path="/auth/home" element={<Home />} />
       <Route path="/auth/register" element={<Register />} />
       <Route path="/auth/forget-password" element={<ForgetPassword />} />
       <Route path="/auth/forget-password-email" element={<ForgetPasswordEmail />} />
-      <Route path="/auth/categories" element={<Categories />} />
     </Routes>
   );
 }
