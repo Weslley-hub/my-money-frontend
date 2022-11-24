edf5bca2faca3e3bf7f0b64cc97eb732da0ee038
Weslley-hub <weslleyfernando71609@gmail.com>
2022-11-23 21:33:25 -0300

    mudança de routes para routes APP, exclução de routes covencional

--- src/routes/App.routes.tsx
+++ src/routes/App.routes.tsx
@@ -1,13 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 
 import { Categories } from "../pages/Categories";
+import { ForgetPassword } from "../pages/ForgetPassword";
+import { ForgetPasswordEmail } from "../pages/ForgetPasswordEmail";
 import { Home } from "../pages/Home";
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/categories" element={<Categories />} />
       <Route path="/" element={<Home />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/register" element={<Register />} />
+      <Route path="/auth/forget-password" element={<ForgetPassword />} />
+      <Route path="/auth/forget-password-email" element={<ForgetPasswordEmail />} />
+      <Route path="/auth/categories" element={<Categories />} />
     </Routes>
   );
 }
